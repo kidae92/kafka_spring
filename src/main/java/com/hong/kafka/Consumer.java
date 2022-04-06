36b4841f65f4b1609aa302a3478d5aee843b9d7d
kidae92 <kidae92@hanmail.net>
2022-04-06 13:30:17 +0900

    Consumer

---
+++ src/main/java/com/hong/kafka/Consumer.java
@@ -0,0 +1,16 @@
+package com.hong.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+    @KafkaListener(id= "hong-id", topics = "test")
+    public void listen(String message){
+        System.out.println("=======");
+        System.out.println(message);
+        System.out.println("=======");
+
+    }
+
+}
